67e1aa2767ebef130c651eb802d2af31c7a08678
nacaru-w <ignaciocr92@gmail.com>
2023-05-16 14:02:24 +0200

    Updating

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+<head>
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<title>Ejemplo básico PHP</title>
+</head>
+<body>
+<?php
+echo 'Hola mundo';
+?>
+</body>
+</html>
