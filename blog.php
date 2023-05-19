2bd6891336af51737684442ba63aa0eaff811ae1
nacaru-w <ignaciocr92@gmail.com>
2023-05-19 02:18:13 +0200

    Finish logic for exercise 2

--- blog.php
+++ blog.php
@@ -1,39 +1,42 @@
 <!DOCTYPE html>
 <html lang="es" dir="ltr">
 <head>
-<meta charset="UTF-8" />
+<meta charset="UTF-8"/>
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-<title>Ejemplo básico PHP</title>
+<title>Blog</title>
 <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<div class="box" style="background-color: pink; font-weight:bold">
-    <?php
-    
-    $prueba = array(1,4,5,6,7);
-    foreach ($prueba as $value) {
-        $value *= 2;
-        echo $value;
-    }
+    <h1>Página principal</h1>
+<div class="blog">
+<?php 
 
-        
-    ?>
-</div>
-<div class "box2" style="background-color: coral; font-weight: bold">
-<?php
 
-$algo = "hola";
-$otraCosa = "qué tal";
-
-function test() {
-    global $algo, $otraCosa;
-    echo $algo.", ".$otraCosa;
-    echo "<br>";
-    echo "<div style=\"font-size:4em;\">mariposa</div>";
-
+function dateParser ($date) {
+    $parsedDate = date("Y-m-d H:i:s", $date);
+    return $parsedDate;
 }
 
-test();
+function textTrimmer ($string) {
+    $wordArray = preg_split('/\s+/', $string);
+    $wordArray120 = array_slice($wordArray, 0, 120);
+    $finalString = implode(' ', $wordArray120);
+    return $finalString;
+}
+
+function turnToSnippet ($title, $date, $summary, $image) {
+    print $imgInHTML = "<div class=\"snippet\"><div class=\"img-container\"><img src=\"{$image}\"></div>";
+    print $titleInHTML = "<h2 class=\"title-teaser\">{$title}</h2>";
+    print $dateInHTML = "<p class=\"date\"> Publicado el " .dateParser($date) ."</p>";
+    print $summaryInHTML = "<p class=\"summary\">" .textTrimmer($summary) ."...<em><a href=\"\"> saber más</a></em></p></div>";
+}
+
+for ($i = 1; $i <= 5; $i++) {
+    $api_url = "api/noticias/es/post_${i}.json";
+    $json= file_get_contents($api_url);
+    $data = json_decode($json);
+    turnToSnippet($data -> title -> es, $data -> date, $data -> description -> es, $data -> image);
+}
 
 ?>
 </div>
