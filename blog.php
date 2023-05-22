febc9717825ea2652893e9389419545348d3e26e
nacaru-w <ignaciocr92@gmail.com>
2023-05-22 22:33:09 +0200

    Finishing exercise 4

--- blog.php
+++ blog.php
@@ -8,9 +8,23 @@
 </head>
 <body>
     <h1>Página principal</h1>
+    
 <div class="blog">
+    <div class="sort-menu">
+        <p>Ordenar:</p>
+        <ul class="sort-options">
+            <li><a href="blog.php?sortType=title">A-Z</a></li>
+            <li><a href="blog.php?sortType=title&sortDir=reverse">Z-A</a></li>
+            <li><a href="blog.php?sortType=date&sortDir=reverse">más reciente</a></li>
+            <li><a href="blog.php?sortType=date">más antiguo</a></li>
+        </ul>
+    </div>
 <?php 
 
+$sortType = $_GET['sortType'];
+$sortDir = $_GET['sortDir'];
+
+$snippetArray = [];
 
 function dateParser ($date) {
     $parsedDate = date("d-m-Y", $date);
@@ -25,17 +39,59 @@ function textTrimmer ($string) {
 }
 
 function turnToSnippet ($title, $date, $summary, $image, $id) {
-    print $imgInHTML = "<div class=\"snippet\"><div class=\"img-container\"><img src=\"{$image}\"></div>";
-    print $titleInHTML = "<h2 class=\"title-teaser\">{$title}</h2>";
-    print $dateInHTML = "<p class=\"date\"> Publicado el " .dateParser($date) ."</p>";
-    print $summaryInHTML = "<p class=\"summary\">" .textTrimmer($summary) ."... <em><a href=\"http://localhost/test/post.php?id={$id}\">saber más</a></em></p></div>";
+
+    $imgInHTML = "<div class=\"snippet\"><div class=\"img-container\"><img src=\"{$image}\"></div>";
+    $titleInHTML = "<h2 class=\"title-teaser\">{$title}</h2>";
+    $dateInHTML = "<p class=\"date\"> Publicado el " .dateParser($date) ."</p>";
+    $summaryInHTML = "<p class=\"summary\">" .textTrimmer($summary) ."... <em><a href=\"post.php?id={$id}\">saber más</a></em></p></div>";
+
+    $finalSnippet = $imgInHTML . $titleInHTML . $dateInHTML . $summaryInHTML;
+
+    print $finalSnippet;
+
 }
 
 for ($i = 1; $i <= 5; $i++) {
     $api_url = "api/noticias/es/post_${i}.json";
-    $json= file_get_contents($api_url);
+    $json = file_get_contents($api_url);
     $data = json_decode($json);
-    turnToSnippet($data -> title -> es, $data -> date, $data -> description -> es, $data -> image, $i);
+
+    switch($sortType) {
+        case 'title':
+            $snippetArray[$i] = $data -> title -> es;
+            continue;
+
+        case 'date':
+            $snippetArray[$i] = $data -> date;
+            continue;
+
+        default:
+            turnToSnippet($data -> title -> es, $data -> date, $data -> description -> es, $data -> image, $i);
+            break;
+    }
+
+}
+
+if ($sortType) {
+
+    function sorter($a, $b) {
+        return $a <=> $b;
+    }
+
+    uasort($snippetArray, "sorter");
+
+    if ($sortDir == 'reverse') {
+        $snippetArray = array_reverse($snippetArray, true);
+    }
+
+    foreach($snippetArray as $key => $value) {
+        $api_url = "api/noticias/es/post_${key}.json";
+        $json = file_get_contents($api_url);
+        $data = json_decode($json);
+
+        turnToSnippet($data -> title -> es, $data -> date, $data -> description -> es, $data -> image, $key);
+
+    }
 }
 
 ?>
