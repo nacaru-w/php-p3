03ea5a59195ea934965d73afc79ef58504db4da1
nacaru-w <ignaciocr92@gmail.com>
2023-05-23 02:04:58 +0200

    Adding term dictionary

---
+++ term_dictionary.php
@@ -0,0 +1,24 @@
+<?php
+$pageTitles = [
+    "main" => [
+        es => "Página principal", 
+        en => "Main page"
+        ],
+    ];
+
+$sortOpts = [
+    "mainLabel" => [
+        es => "Ordenar",
+        en => "Sort"
+    ],
+    "recentLabel" => [
+        es => "más reciente",
+        en => "most recent"
+    ],  
+    "oldLabel" => [
+        es => "más antiguo",
+        en => "oldest"
+    ]
+];
+
+?>
